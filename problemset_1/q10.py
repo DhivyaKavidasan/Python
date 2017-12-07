c5146e76d806a1b381df02f35bb9cc131cc380fe
Kavidasan <dhivya.kavidasan@accenture.com>
2017-12-07 12:23:16 +0530

    added

---
+++ problemset_1/q10.py
@@ -0,0 +1,46 @@
+'''
+code snippet
+submitted by : dhivya.kavidasan
+date: 05/12/2017
+'''
+#a. What would the code above return if the statement x = 25 were replaced by x = -25?
+x = 25
+epsilon = 0.01
+numGuesses = 0
+low = 0.0
+high = max(1.0, x)
+ans = (high + low)/2.0
+while abs(ans**2 - x) >= epsilon:
+  print 'low =', low, 'high =', high, 'ans =', ans
+  numGuesses += 1
+  if ans**2 < x:
+    low = ans
+  else:
+    high = ans
+    ans = (high + low)/2.0
+print 'numGuesses =', numGuesses
+print ans, 'is close to square root of', x
+
+It results in infinite loop.
+
+
+#b. What would have to be changed to make the code above for finding an approximation to the cube root of both negative and positive numbers?
+x = 25
+epsilon = 0.01
+numGuesses = 0
+low = 0.0
+high = max(1.0, x)
+ans = (high + low)/2.0
+while abs(ans**3 - x) >= epsilon:
+  print 'low =', low, 'high =', high, 'ans =', ans
+  numGuesses += 1
+  if ans**3 < x:
+    low = ans
+  else:
+    high = ans
+    ans = (high + low)/2.0
+print 'numGuesses =', numGuesses
+print ans, 'is close to square root of', x
+
+
+
